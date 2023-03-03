5492574c048702d52b0c3122f9b6cfc1cf28dcd7
unknown <you@example.com>
2023-03-03 14:37:41 +1100

    juan-phone with svg. Better quality

--- src/partials/Introduction.jsx
+++ src/partials/Introduction.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import IphoneMockup from '../images/iphone-mockup.png';
-import JuanPhone from "../images/Juan-phone.webp"
+import JuanPhone from "../images/Juan-phone.svg"
 import Resume from "../assets/resume.pdf"
 
 
