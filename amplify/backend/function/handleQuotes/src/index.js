e37c131b3000ac2cb118be9c857f1c3e4874318f
unknown <you@example.com>
2023-03-08 09:14:00 +1100

    params2 added to backend. Now, we can send mails to any account

--- amplify/backend/function/handleQuotes/src/index.js
+++ amplify/backend/function/handleQuotes/src/index.js
@@ -27,13 +27,35 @@ exports.handler = async (event) => {
                 Data: `Message request.`
             },
         },
-        Source: "sernadominguezj@gmail.com"
+        Source: "no-reply@juansernad.com"
+    };
+
+    // Send email to client/recruiter
+    const params2 = {
+        Destination: {
+            ToAddresses: [`${parsedBody.email}`]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: `Dear ${parsedBody.name}, thank you for your message. `
+                },
+                Html: {
+                    Data: `<html><head><title>Message confirmation</title><style>*{margin:0; padding:0; box-sizing:border-box;} header{height: 65px; background-color:#1D1D20; color:white; text-align:center; padding-top:13px;} #main{padding-top:20px; padding-bottom: 20px; padding-left:10px; padding-right:10px; background-color:#D3D3D3;} h1{font-size:1.5rem;} footer{height:90px; background-color:#1D1D20; color:white; text-align:center; padding-top:10px; padding-bottom:10px;}</style></head><body><header><h1>Message confirmation</h1></header><div id="main"><h2><strong>Hello ${parsedBody.name},</strong></h2><br/><p>Thanks for your message, I will be in touch soon!.</p><br/><p>Meanwhile, please see your message details: ${parsedBody.message}.</p><br/><p>Best regards,</p><p>Juan Serna Dev.</p></div><footer>Juan Serna Web Developer<br/>Copyright 2023<br/>Contact us: +61 414029043</footer></body></html>`
+                }
+            },
+            Subject: {
+                Data: `Message confirmation`
+            },
+        },
+        Source: "no-reply@juansernad.com"
     };
 
     try {
         const result = await SES.sendEmail(params).promise();
+        const result2 = await SES.sendEmail(params2).promise();
 
-        console.log(result);
+        console.log(result, result2);
 
         return {
             statusCode: 200,
