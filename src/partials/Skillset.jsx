1a34b1de6c350429dae35c374b0840a8f8b6df0e
unknown <you@example.com>
2023-08-23 11:39:25 +1000

    Working on skillset

--- src/partials/Skillset.jsx
+++ src/partials/Skillset.jsx
@@ -2,6 +2,7 @@ import { motion, useAnimation } from 'framer-motion';
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom';
+import { Collapse, Typography } from 'antd';
 
 const SkillSet = () => {
   
@@ -20,7 +21,6 @@ const SkillSet = () => {
           });
       }
   }, [isParentInView, parentControls]);
-
   
   return ( 
     <section ref={ParentRef} className="border-t border-transparent dark:border-gray-800">
@@ -87,9 +87,6 @@ const SkillSet = () => {
                   <p className='text-xl text-gray-600 dark:text-gray-400 text-center'>Github</p>
                 </li>
                 <li>
-                  <p className='text-xl text-gray-600 dark:text-gray-400 text-center'>VS Code</p>
-                </li>
-                <li>
                   <p className='text-xl text-gray-600 dark:text-gray-400 text-center'>NPM</p>
                 </li>
                 <li>
@@ -106,9 +103,6 @@ const SkillSet = () => {
                 </li>
                 <li>
                   <p className='text-xl text-gray-600 dark:text-gray-400 text-center'>SEO optimization</p>
-                </li>
-                <li>
-                  <p className='text-xl text-gray-600 dark:text-gray-400 text-center'>Google Search Console</p>
                 </li>
               </ul>
             </div>
@@ -142,7 +136,7 @@ const SkillSet = () => {
             </div>
 
             <div className='col-span-3 md:col-span-2 mx-auto'>
-            <svg className="h-12 w-12 dark:text-white mx-auto animate-pulse"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/></svg>
+              <svg className="h-12 w-12 dark:text-white mx-auto animate-pulse"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/></svg>
 
               <p className='text-2xl text-gray-600 text-center font-poppins dark:text-white mt-2'>Project management</p>
               <ul>
@@ -177,6 +171,9 @@ const SkillSet = () => {
               </ul>
             </div>
           </div>
+
+
+
         </div>
         <div className="mt-12 font-poppins max-w-3xl mx-auto flex justify-center">
           <Link to='/projects'>
